8ada06bfbe2ed3259f2b56e3e3ac916e436ed3d6
Bowofade Oyerinde <bowofadeoyerinde@gmail.com>
2024-01-02 22:21:53 +0100

    vendor orders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import "./App.css";
+import "./index.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="ade">I am learning React for the first time</h1>
+        <h1 className="text-2xl font-medium text-green-500 text-center">
+          I am learning React for the first time
+        </h1>
       </header>
+      <div className="w-full max-w-5xl rounded-xl shadow-sm p-4 mx-auto bg-gray-100 m-4">
+        <h1 className="text-2xl text-center font-medium text-blue-500 capitalize">
+          My First react text with tailwindcss styling
+        </h1>
+      </div>
     </div>
   );
 }
